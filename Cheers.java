6dc969f8648eefe0428bd19aa45101642f767049
tidharhayute-runi <tidhar.hayute@post.runi.ac.il>
2024-11-19 08:50:55 +0200

    Submit

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,19 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0].toUpperCase();
+        int crowdAmount = Integer.parseInt(args[1]);
+
+        for (int i = 0; i < word.length(); i++) {
+            if("AEFHILMNORSX".indexOf(word.charAt(i)) != -1) {
+                System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) +"!");
+            } else System.out.println("Give me a " + word.charAt(i) + ": " + word.charAt(i) +"!");
         }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < crowdAmount; i++) {
+            System.out.println(word + "!!!");
+        }
+    }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,42 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+
+		int steps = 1; 
+		int currentValue = 0;
+		int success = 0;
+
+		for (int i = 0; i < seed; i++) {
+			currentValue = i + 1;
+			if (mode.equals("v")) System.out.print(currentValue + " ");
+			currentValue = (currentValue % 2 == 0) ? (currentValue / 2) : ((currentValue * 3) + 1);
+			
+			steps = 2;
+			while (currentValue != 1) {
+				if (mode.equals("v")) System.out.print(currentValue + " ");
+				currentValue = (currentValue % 2 == 0) ? (currentValue / 2) : ((currentValue * 3) + 1);
+				steps++;
+			} 
+
+			if (currentValue == 1) {
+				success++;
+
+				if (mode.equals("v")) {
+					System.out.print(currentValue + " ");
+					System.out.print("(" + steps + ")");
+					System.out.println("");
+				}
+			}
+
+			
+		}
+
+		if (mode.equals("c")) {
+			if (success == seed){
+				System.out.println("Every one of the first " + success + " hailstone sequences reached 1.");
+			}
+		}
 	}
 }
