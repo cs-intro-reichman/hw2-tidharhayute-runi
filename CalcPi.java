2a483e0521eafef5afc190445af3fe01adf6df6c
tidharhayute-runi <tidhar.hayute@post.runi.ac.il>
2024-11-19 09:16:19 +0200

    Submit

--- CalcPi.java
+++ CalcPi.java
@@ -10,6 +10,6 @@ public class CalcPi {
 		}
 
 		System.out.println("pi according to Java: " + Math.PI );
-		System.out.println("pi, approximated:	 " + sum * 4);
+        System.out.printf("pi, approximated:\t", sum * 4);
 	}
 }
